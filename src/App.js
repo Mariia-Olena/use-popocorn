529ee6a3056c2cd4cef6bed406d0bce876d6e629
Mariia-Olena <mariia.stus@gmail.com>
2024-01-04 14:54:13 +0200

    add clean-up functions

--- src/App.js
+++ src/App.js
@@ -62,25 +62,31 @@ export default function App() {
 
   useEffect(
     function () {
+      const controller = new AbortController();
+
       async function fetchMovies() {
         try {
           setIsLoading(true);
           setError('');
+
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
           );
 
           if (!res.ok)
             throw new Error('Something went wrong with fetching movies');
 
           const data = await res.json();
-
           if (data.Response === 'False') throw new Error('Movie not found');
 
           setMovies(data.Search);
+          setError('');
         } catch (err) {
           console.error(err.message);
-          setError(err.message);
+          if (err.name !== 'AbortError') {
+            setError(err.message);
+          }
         } finally {
           setIsLoading(false);
         }
@@ -93,6 +99,10 @@ export default function App() {
       }
 
       fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
     },
     [query]
   );
@@ -194,6 +204,18 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       getMovieDetails();
     },
     [selectedId]
+  );
+
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = 'usePopcorn';
+      };
+    },
+    [title]
   );
 
   function handleAdd() {
